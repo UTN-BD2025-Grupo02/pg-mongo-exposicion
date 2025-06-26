cc6d4d6b4f6b37dfbe16c97f54626acc3168797c
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-06-26 19:26:03 -0300

    Add CiudadModel export to ciudad.schema.ts for Mongoose integration

--- src/schemas/ciudad.schema.ts
+++ src/schemas/ciudad.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 
 
 @Schema({collection: 'ciudades'})
@@ -12,3 +12,4 @@ export class Ciudad extends Document {
 }
 
 export const CiudadSchema = SchemaFactory.createForClass(Ciudad);
+export const CiudadModel = mongoose.model(Ciudad.name, CiudadSchema);
