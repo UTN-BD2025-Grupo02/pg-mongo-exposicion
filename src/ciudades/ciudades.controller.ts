41a2b09e7e4e43b4be35225c4486134de356270f
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-06-30 20:27:06 -0300

    Add CiudadesController, CiudadesService, and CreateCiudadDto for city management

---
+++ src/ciudades/ciudades.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CiudadesService } from './ciudades.service';
+import { CreateCiudadDto } from '../interfaces/createCiudad.dto';
+import { CiudadEntity } from '../entities/ciudad.entity';
+
+@Controller('ciudades')
+export class CiudadesController {
+  constructor(private readonly ciudadesService: CiudadesService) {}
+
+  @Post()
+  create(@Body() createCiudadDto: CreateCiudadDto): Promise<CiudadEntity> {
+    return this.ciudadesService.create(createCiudadDto);
+  }
+
+  @Get('all')
+  findAll(): Promise<CiudadEntity[]> {
+    return this.ciudadesService.findAll();
+  }
+
+  @Get(':nombre')
+  findByName(@Param('nombre') nombre: string): Promise<CiudadEntity> {
+    return this.ciudadesService.findByName(nombre);
+  }
+
+  @Delete(':nombre')
+  delete(nombre: string): Promise<{message: string}> {
+    return this.ciudadesService.delete(nombre);
+  }
+}
