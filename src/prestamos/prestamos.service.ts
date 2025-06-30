476ec988b74312de1f78a37006d22e6ce48d819f
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-06-30 20:09:59 -0300

    Implement PrestamosController and PrestamosService with findAll method

---
+++ src/prestamos/prestamos.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { In, MongoRepository, Repository } from 'typeorm';
+import { PrestamoEntity } from '../entities/prestamo.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EstadoPrestamoEntity } from '../entities/estadoPrestamo.entity';
+import { LectorEntity } from '../entities/lector.entity';
+import { ObjectId } from 'mongodb';
+
+@Injectable()
+export class PrestamosService {
+  private prestamoRepository: Repository<PrestamoEntity>;
+  private estadoRepository: MongoRepository<EstadoPrestamoEntity>;
+  private lectorRepository: MongoRepository<LectorEntity>;
+
+  constructor(
+    @InjectRepository(PrestamoEntity)
+    prestamoRepository: Repository<PrestamoEntity>,
+
+    @InjectRepository(EstadoPrestamoEntity)
+    estadoRepository: MongoRepository<EstadoPrestamoEntity>,
+
+    @InjectRepository(LectorEntity)
+    lectorRepository: MongoRepository<LectorEntity>,
+  ) {
+    this.prestamoRepository = prestamoRepository;
+    this.estadoRepository = estadoRepository;
+    this.lectorRepository = lectorRepository;
+  }
+
+  async findAll(): Promise<any[]> {
+    const prestamos = await this.prestamoRepository.find();
+
+    const lectoresIds = prestamos.map((p) => p.lector);
+    const estadosIds = prestamos.map((p) => p.estado);
+
+
+
+    const lectores = await this.lectorRepository.find({
+      where: { _id: {$in: lectoresIds} },
+    });
+    const estados = await this.estadoRepository.find({
+      where: { _id: {$in: estadosIds} },
+    });
+
+
+
+    return prestamos.map((prestamo) => ({
+      ...prestamo,
+      lector: lectores.find((l) => l._id.equals(prestamo.lector)),
+      estado: estados.find((e) => e._id.equals(prestamo.estado)),
+    }));
+  }
+}
